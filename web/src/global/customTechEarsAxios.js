77688a8661a1cc394c630f0025ec3cbd8e7b9623
Gege <gausskinneas@gmail.com>
2024-07-11 20:50:57 -0300

    verdaovaiganhar

--- web/src/global/customTechEarsAxios.js
+++ web/src/global/customTechEarsAxios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const customTechearsAxios = axios.create({
-  baseURL: 'http://localhost:8911/graphql',
+  baseURL: 'http://localhost:8912/graphql',
 });
 
 // Adicione um interceptor de requisição
